dea3def2be8c5fe5aed489e722c2556e347694b1
lauragonzs <lauragonzsuarez@gmail.com>
2023-06-22 00:29:14 +0100

    myProjects's toggle added and new logos uploaded

--- myPortfolioProject/src/app/projects/projects.component.ts
+++ myPortfolioProject/src/app/projects/projects.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  showDescription1 = false;
+  showDescription2 = false;
+  showDescription3 = false;
+  showDescription4 = false;
+  showDescription5 = false;
+  showDescription6 = false;
 
+  toggleDescription(item:number) {
+    if (item == 1){
+      this.showDescription1 = !this.showDescription1;
+    }
+    else if (item == 2){
+      this.showDescription2 = !this.showDescription2;
+    }
+    else if (item == 3){
+      this.showDescription3 = !this.showDescription3;
+    }
+    else if (item == 4){
+      this.showDescription4 = !this.showDescription4;
+    }
+    else if (item == 5){
+      this.showDescription4 = !this.showDescription4;
+    }
+    else if (item == 6){
+      this.showDescription4 = !this.showDescription4;
+    }
+  }
 }
